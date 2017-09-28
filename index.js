82b0ed1e25338866913556ca81b99a922c01c3b3
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 06:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newobj = object
+  var newobj = Object.assign({}, object)
   newobj[key] = value;
   return newobj
 }
