0d46a8757f34a672e65eeadc6bfb259d2e10b09e
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 06:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
   object[key] = value;
   return object
 }
+
+function deleteFromObjectByKey(object, key)
+{
+  var newobj = Object.assign({}, object)
+  delete newobj[key]
+  return newobj
+}
